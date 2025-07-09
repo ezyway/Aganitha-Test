545d80bbfef2c699a0bcf388aa6128808ca19808
Sreyas <sreyas.codaw@gmail.com>
2025-07-09 13:47:57 +0530

    added

---
+++ src/get-papers-list.py
@@ -0,0 +1,14 @@
+import requests
+
+class PubMedFetcher:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.base_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/"
+        
+
+def main():
+    pass
+    
+
+if __name__ == "__main__":
+    main()
